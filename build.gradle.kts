0403a73f12ec906499df163e01b0e28617a7fe6f
Bruno Vicenzo <bruno@vicenzo.me>
2021-01-11 11:38:15 -0300

    Using right jackson library

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("io.javalin:javalin:3.12.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.9")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.+")
     testImplementation(kotlin("test-junit"))
 }
 
